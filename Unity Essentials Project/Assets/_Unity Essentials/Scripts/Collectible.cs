c0cdf48fefe55e8e40fd6c7a28035c69363a4cf9
Theinterna <yalvin58@gmail.com>
2024-06-29 16:57:48 -0700

    Add visual effects for collectible
    Also changed camera angle and player speed so effects can be seen more clearly

--- Unity Essentials Project/Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Unity Essentials Project/Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -6,6 +6,8 @@ public class Collectible : MonoBehaviour
 {
     // public makes it editable in the inspector, private will not
     public float rotationSpeed = 0.5f;
+    // create an effect gameObject for when the player collects an object
+    public GameObject onCollectEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +20,11 @@ public class Collectible : MonoBehaviour
         // rotates half a degree in each frame by default (can be changed though)
         // we need the f if it's a decimal number bc C# defines them as floats
         transform.Rotate(0,rotationSpeed,0);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Instantiate(onCollectEffect,transform.position,transform.rotation);
+        // Destroy the collectible
+        Destroy(gameObject);
     }
 }
